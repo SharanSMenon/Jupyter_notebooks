3b0fe8962d448a3efe65aac6aadd1a7e8071a3fd
SharanSMenon <sharansajivmenon@gmail.com>
2018-03-22 11:04:23 -0400

    =More files

---
+++ Java_Course/KeywordsAndExpressions/src/com/sharans/Methods.java
@@ -0,0 +1,7 @@
+package com.sharans;
+
+public class Methods {
+    public static void main(String[] args){
+        //Methods
+    }
+}
--- Java_Course/KeywordsAndExpressions/src/com/sharans/Statements.java
+++ Java_Course/KeywordsAndExpressions/src/com/sharans/Statements.java
@@ -2,6 +2,20 @@ package com.sharans;
 
 public class Statements {
     public static void main(String[] args) {
-        
+        //Statements, Whitespace, and indenting
+        boolean gameOver = true;
+        int score = 5001;
+        int levelC = 5;
+        int bonus = 100;
+        calculateScore(gameOver,score,levelC,bonus);
+        calculateScore(true,2852,9,300);
+    }
+    public static int calculateScore(boolean gameOver, int score, int levelC, int bonus) {
+        if (gameOver) {
+            int finalscore = score + (levelC * bonus);
+            System.out.println("Score was " + finalscore);
+            return finalscore;
+        }
+        return -1;
     }
 }
