c9eb270b2124b151c7723cabd429488025e2c72d
SharanSMenon <sharansajivmenon@gmail.com>
2018-03-24 13:18:25 -0400

    Did some work here and there. Also named some files

--- Java_Course/KeywordsAndExpressions/src/com/sharans/Statements.java
+++ Java_Course/KeywordsAndExpressions/src/com/sharans/Statements.java
@@ -7,15 +7,41 @@ public class Statements {
         int score = 5001;
         int levelC = 5;
         int bonus = 100;
-        calculateScore(gameOver,score,levelC,bonus);
-        calculateScore(true,2852,9,300);
+        int finals = calculateScore(gameOver,score,levelC,bonus);
+//        calculateScore(true,2852,9,300);
+        int position = calculateHighScorePosition(1499);
+        displayHighScorePosition("Sharan",position);
+
+        position = calculateHighScorePosition(70);
+        displayHighScorePosition("Jahnvi",position);
+
+        position = calculateHighScorePosition(256);
+        displayHighScorePosition("Anu",position);
+
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Sajiv",position);
+        System.out.println("Score is now "+finals);
     }
     public static int calculateScore(boolean gameOver, int score, int levelC, int bonus) {
         if (gameOver) {
             int finalscore = score + (levelC * bonus);
-            System.out.println("Score was " + finalscore);
+            finalscore += 2000;
             return finalscore;
         }
         return -1;
+    }
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " got in the "+position+" position");
+    }
+    public static int calculateHighScorePosition(int score){
+        if(score >= 1000){
+            return 1;
+        }else if(score >= 500 && score < 1000){
+            return 2;
+        }else if(score >= 100 && score < 500){
+            return 3;
+        }else{
+            return 4;
+        }
     }
 }
