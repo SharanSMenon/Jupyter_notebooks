50482ef8108d7f68ce65c5f609c7853cb2f8de06
SharanSMenon <sharansajivmenon@gmail.com>
2018-03-30 16:11:05 -0400

    commit from PyCharm

--- Projects/PythonFunctions/SFunctions/numbers.py
+++ Projects/PythonFunctions/SFunctions/numbers.py
@@ -1,6 +1,9 @@
 def isPrime(x):
-    for i in range(0,x//2):
-        if x % 2 == 0:
+    for i in range(2, x // 2):
+        if x % i == 0:
             return False
     return True
+
+
 isEven = lambda x: x % 2 == 0
+isOdd = lambda x: x % 2 == 1
