91866d2e2b5d3e286ed5959ec9d869f247fb6b49
SharanSMenon <sharansajivmenon@gmail.com>
2018-03-30 16:12:44 -0400

    Busnkfjo

---
+++ Projects/JavaFunctions/src/com/sharan/Main.java
@@ -0,0 +1,10 @@
+package com.sharan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Numbers ntest = new Numbers();
+        System.out.println("10 is prime: " + ntest.isPrime(10));
+        System.out.println("5 is prime: " + ntest.isPrime(5));
+    }
+}
---
+++ Projects/JavaFunctions/src/com/sharan/Numbers.java
@@ -0,0 +1,22 @@
+package com.sharan;
+
+public class Numbers {
+    public Numbers() {
+        System.out.println("Use the methods provided by this class");
+        System.out.println("Enter your number");
+    }
+    public boolean isEven(int x){
+        return x % 2 == 0;
+    }
+    public boolean isOdd(int x){
+        return x % 2 == 1;
+    }
+    public boolean isPrime(int x){
+        for (int i = 2;i < x/2;i++){
+            if (i % 2 == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
